5761e70e299105a1430fe09c785721747985dc08
ridingrails <stancompedu@yahoo.com>
2015-08-04 18:55:08 -0700

    fixed mongo

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 #require "active_resource/railtie" # Comment this line for Rails 4.0+
-#require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
