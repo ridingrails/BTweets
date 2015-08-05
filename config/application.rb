abb7f8078dd91463ffa5bc8a2e4a1fb3b367cd3b
ridingrails <stancompedu@yahoo.com>
2015-08-04 19:28:26 -0700

    mongoid config file

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ Bundler.require(*Rails.groups)
 
 module BTweets
   class Application < Rails::Application
-    Mongoid.load!("config/mongoid.yml")
+    Mongoid.load!("mongoid.yml")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
